f7901dec19b5ee666e873a4981ccf3d4b3dcbcb7
Paula Sousa <paulasousa137@gmail.com>
2023-04-03 18:31:39 +0200

    Seleccion por teclado ejercicio 8

---
+++ ta20_8/src/main/java/c4/ta20_8/App.java
@@ -0,0 +1,17 @@
+package c4.ta20_8;
+
+/**
+ * Mejorar la aplicación grafica del ejercicio anterior. Además de las funcionalidades anteriores,
+ * se debe permitir operar con los botones desde el teclado y hay que añadir un botón que permita 
+ * borrar los campos de datos y resultado. También para darle mayor robustez, puedes incluir un control
+ *  de errores que avise mediante una ventana emergente si se ha introducido un número en formato erróneo,
+ *  evitando que la aplicaión se detenga.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ejercicio8 frame = new ejercicio8();
+    }
+}
---
+++ ta20_8/src/main/java/c4/ta20_8/ejercicio8.java
@@ -0,0 +1,119 @@
+package c4.ta20_8;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class ejercicio8 extends JFrame {
+
+	private JPanel contentPane;
+
+	private JTextField cantidad;
+	private JTextField resultado;
+	private JRadioButton rdbtnEurosPesetas = new JRadioButton();
+	JRadioButton rdbtnPesetasEuros = new JRadioButton();
+	ButtonGroup grupoMoneda = new ButtonGroup();
+	JButton btnCalcular = new JButton();
+
+	/**
+	 * Create the frame.
+	 */
+	public ejercicio8() {
+		setTitle("Calculadora de cambio de monedas");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 328);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setVisible(true);
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblCantidad = new JLabel("Cantidad a convertir");
+		lblCantidad.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblCantidad.setBounds(10, 26, 197, 42);
+		contentPane.add(lblCantidad);
+
+		cantidad = new JTextField();
+		cantidad.setBounds(205, 28, 96, 42);
+		contentPane.add(cantidad);
+		cantidad.setColumns(10);
+		cantidad.addKeyListener(kA);
+
+		JLabel lblResultado = new JLabel("Resultado");
+		lblResultado.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblResultado.setBounds(116, 208, 117, 42);
+		contentPane.add(lblResultado);
+
+		resultado = new JTextField();
+		resultado.setColumns(10);
+		resultado.setBounds(243, 210, 96, 42);
+		contentPane.add(resultado);
+
+		rdbtnEurosPesetas = new JRadioButton("Euros a Pesetas");
+		rdbtnEurosPesetas.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		rdbtnEurosPesetas.setBounds(40, 98, 179, 23);
+		grupoMoneda.add(rdbtnEurosPesetas);
+		contentPane.add(rdbtnEurosPesetas);
+
+		rdbtnPesetasEuros = new JRadioButton("Pesetas a Euros");
+		rdbtnPesetasEuros.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		rdbtnPesetasEuros.setBounds(255, 98, 152, 23);
+		grupoMoneda.add(rdbtnPesetasEuros);
+		contentPane.add(rdbtnPesetasEuros);
+
+		btnCalcular = new JButton("Calcular");
+		btnCalcular.addActionListener(aL);
+		btnCalcular.setBackground(new Color(216, 207, 220));
+		btnCalcular.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnCalcular.setBounds(155, 149, 117, 31);
+		contentPane.add(btnCalcular);
+		btnCalcular.addKeyListener(kA);
+	}
+
+	ActionListener aL = new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			double cantidadAConvertir = Double.parseDouble(cantidad.getText());
+			double resultadoConversion = 0.0;
+			if (rdbtnEurosPesetas.isSelected()) {
+				resultadoConversion = cantidadAConvertir * 166.386; // 1 euro = 166.386 pesetas
+				resultado.setText(String.format("%.2f", resultadoConversion));
+			} else if (rdbtnPesetasEuros.isSelected()) {
+				resultadoConversion = cantidadAConvertir / 166.386; // 1 peseta = 0.006 euros
+				resultado.setText(String.format("%.2f", resultadoConversion));
+			}
+
+		}
+	};
+	
+	KeyAdapter kA = new KeyAdapter() {
+	    @Override
+	    public void keyPressed(KeyEvent e) {
+	        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+	            btnCalcular.doClick();
+	            
+	        } else if (e.getKeyCode() == KeyEvent.VK_TAB && e.getModifiers() == KeyEvent.SHIFT_DOWN_MASK) {
+	            // Selección del radio button "Pesetas a Euros" con Shift + Tab
+	            rdbtnPesetasEuros.setSelected(true);
+	        } else if (e.getKeyCode() == KeyEvent.VK_TAB) {
+	            // Selección del radio button "Euros a Pesetas" con Tab
+	            rdbtnEurosPesetas.setSelected(true);
+	        
+	    }
+	    }
+	};
+
+}
