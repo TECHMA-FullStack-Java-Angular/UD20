17100891c4dc563d88bd81a8d40e2de41cecc888
Paula Sousa <paulasousa137@gmail.com>
2023-04-03 10:24:05 +0200

    Ejercicio 1

--- ta20_1/src/main/java/c4/ta20_1/App.java
+++ ta20_1/src/main/java/c4/ta20_1/App.java
@@ -1,8 +1,8 @@
 package c4.ta20_1;
 
 /**
- * Ejercicio 1: Escribir una aplicación con interfaz gráfica en la que se contruya una ventana con título
- *  y maarco que tenga los controles básicos (es decir, restaurar, maximizar y cerrar) y que al pulsar sobre 
+ * Ejercicio 1: Escribir una aplicación con interfaz gráfica en la que se construya una ventana con título
+ *  y marco que tenga los controles básicos (es decir, restaurar, maximizar y cerrar) y que al pulsar sobre 
  *  el aspa de la ventana (cerrar) se salga completamente de la aplicación. La ventana contendrá una etiqueta
  *  y el usuario debe poder cambiar su tamaño.
  *
@@ -11,6 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Ventana ventana = new Ventana();
     }
 }
--- ta20_1/src/main/java/c4/ta20_1/Ventana.java
+++ ta20_1/src/main/java/c4/ta20_1/Ventana.java
@@ -4,11 +4,22 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRootPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 public class Ventana extends JFrame {
 
 	private JPanel contentPane;
+	 private JLabel etiqueta;
+	/**
+	 * @wbp.nonvisual location=351,29
+	 */
+
 
 	
 
@@ -16,13 +27,50 @@ public class Ventana extends JFrame {
 	 * Create the frame.
 	 */
 	public Ventana() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		initComponents();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 
-		setVisible(true);
-		setContentPane(contentPane);
+    private void initComponents() {
+        etiqueta = new JLabel("¡Soy una etiqueta!");
+        etiqueta.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
+        getContentPane().add(etiqueta);
+
+        setTitle("Ventana redimensionable");
+        setResizable(true);
+        setBounds(100, 100, 400, 300);
+
+        JRootPane rootPane = SwingUtilities.getRootPane(etiqueta);
+        rootPane.setWindowDecorationStyle(JRootPane.FRAME);
+    
+		
+		
+		
+		
+//		setTitle("Control básico");
+//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setBounds(100, 100, 450, 300);
+//		contentPane = new JPanel();
+//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//		
+//		
+//
+//		setVisible(true);
+//		setContentPane(contentPane);
+//		contentPane.setLayout(null);
+//		
+//		JLabel lblNewLabel = new JLabel("¡Soy redimensionable!");
+//		
+//		getContentPane().add(lblNewLabel);
+//		setTitle("Etiqueta");
+//		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+//		lblNewLabel.setBounds(91, 90, 275, 36);
+//		setResizable(true);
+//		contentPane.add(lblNewLabel);
 	}
 
 }
