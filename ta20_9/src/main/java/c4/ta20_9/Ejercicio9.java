c82a20bf99d1401f1dbf17ea4f356ef7f4568541
Paula Sousa <paulasousa137@gmail.com>
2023-04-05 20:31:42 +0200

    logica con errores

--- ta20_9/src/main/java/c4/ta20_9/Ejercicio9.java
+++ ta20_9/src/main/java/c4/ta20_9/Ejercicio9.java
@@ -6,38 +6,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JToggleButton;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class Ejercicio9 extends JFrame {
 
 	private JPanel contentPane;
-	private JToggleButton tglbtn1;
-	private JToggleButton tglbtn2;
-	private JToggleButton tglbtn3;
-	private JToggleButton tglbtn4;
-	private JToggleButton tglbtn5;
-	private JToggleButton tglbtn6;
-	private JToggleButton tglbtn7;
-	private JToggleButton tglbtn8;
-	private JToggleButton tglbtn9;
-	private JToggleButton tglbtn10;
-	private JToggleButton tglbtn11;
-	private JToggleButton tglbtn12;
-	private JToggleButton tglbtn13;
-	private JToggleButton tglbtn14;
-	private JToggleButton tglbtn15;
-	private JToggleButton tglbtn16;
-	private ArrayList<Integer> selectedButtons;
+	private int selectedButtons;
 	private ArrayList<Color> colores;
 	private ArrayList<JToggleButton> listaBotones;
+	private JToggleButton botonSeleccionado1 = new JToggleButton();
+	private JToggleButton botonSeleccionado2 = new JToggleButton();
 	private int parejasEncontradas;
-	private JToggleButton botonAnterior;
+	private Timer timer = new Timer();
 	private int intentos = 0;
 	private JToggleButton b;
+	private JLabel lblIntentos = new JLabel();
 	JToggleButton[][] cartas = new JToggleButton[4][4];
 
 	/**
@@ -45,22 +37,26 @@ public class Ejercicio9 extends JFrame {
 	 */
 	public Ejercicio9() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 515, 550);
+		setBounds(100, 100, 515, 682);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
+		lblIntentos = new JLabel("Intentos:");
+		lblIntentos.setFont(new Font("Calibri", Font.BOLD, 16));
+		lblIntentos.setBounds(34, 458, 130, 33);
+		contentPane.add(lblIntentos);
+
 		// Inicializar las variables
 		colores = new ArrayList<Color>();
 		listaBotones = new ArrayList<JToggleButton>();
-		selectedButtons = new ArrayList<>();
+		selectedButtons = 0;
 		parejasEncontradas = 0;
-		botonAnterior = null;
 		
-		
-		//crear cartas
+
+		// crear cartas
 		int px = 60;
 		int py = 60;
 		final int SIZE = 90;
@@ -72,7 +68,8 @@ public class Ejercicio9 extends JFrame {
 				listaBotones.add(cartas[j][i]);
 				cartas[j][i].setEnabled(true);
 				cartas[j][i].addActionListener(aL);
-				
+				cartas[j][i].setBackground(Color.lightGray);
+
 				px += 95;
 			}
 			px = 60;
@@ -80,7 +77,7 @@ public class Ejercicio9 extends JFrame {
 		}
 
 		// Agregar los colores de las parejas
-		
+
 		colores.add(Color.RED);
 		colores.add(Color.RED);
 		colores.add(Color.BLUE);
@@ -100,14 +97,7 @@ public class Ejercicio9 extends JFrame {
 
 		// Barajar los colores
 		Collections.shuffle(colores);
-		
 
-		//Establece colores a las cartas
-		for (int i = 0; i < listaBotones.size(); i++) {
-		    listaBotones.get(i).setBackground(colores.get(i));
-		    listaBotones.get(i).setSelected(true);
-		}
-		
 		
 
 		setVisible(true);
@@ -116,26 +106,59 @@ public class Ejercicio9 extends JFrame {
 	ActionListener aL = new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			b = (JToggleButton) e.getSource();
-		
 
-			if (b.isSelected()) {
-				
-				
-				selectedButtons.add(listaBotones.indexOf(b));
-				
-				
-					
-					
-
-				
-				
-				
-				
-
+			// Establece colores a las cartas
+			for (int i = 0; i < listaBotones.size(); ) {
+				b.setBackground(colores.get(i));
+				b.setSelected(false);
+				colores.remove(i);
+				break;
 			}
 
+				selectedButtons++;
+				if (selectedButtons<2) {
+					botonSeleccionado1=b;
+				}
+				
+				if (selectedButtons ==2) {
+					botonSeleccionado2=b;
+					intentos++;
+					if (botonSeleccionado1.getBackground().equals(botonSeleccionado2
+							.getBackground())) {
+						botonSeleccionado1.setEnabled(false);
+						botonSeleccionado2.setEnabled(false);
+						botonSeleccionado1=null;
+						botonSeleccionado2=null;
+						selectedButtons=0;
+						parejasEncontradas++;
+						
+						if (parejasEncontradas == 8) {
+							System.out.println("¡Felicidades! ¡Ganaste en " + intentos + " intentos!");
+						}
+					} else {
+						
+						timer.schedule(new TimerTask() {
+						    @Override
+						    public void run() {
+						        botonSeleccionado1.setSelected(false);
+						        botonSeleccionado2.setSelected(false);
+						        botonSeleccionado1.setBackground(Color.LIGHT_GRAY);
+						        botonSeleccionado2.setBackground(Color.LIGHT_GRAY);
+						        botonSeleccionado1.setEnabled(true);
+						        botonSeleccionado2.setEnabled(true);
+						        botonSeleccionado1 = null;
+						        botonSeleccionado2 = null;
+						        selectedButtons = 0;
+						    }
+						}, 3000);
+						
+					
+
+					}
+				}
+
+			
 
 		}
 	};
 }
-
